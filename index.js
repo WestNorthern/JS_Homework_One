2b1545e6a7bb31bc4d3d7db954235f3f3107f83d
Ryan <rrichardson610@gmail.com>
2017-10-04 16:58:57 -0600

    Added boilerplate to index.html and index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+// Create a script with two variables, each assigned to a number. 
+// Add them together and output the result to the console. Now do the same with two strings (concatenation).
+// One
+
+
+// Create a multidimensional array. The array will be an array of Aston Martin cars, 
+// BMW cars, and Bentley cars, 3 cars of each make.
+// Two
+
+
+// Write a script that checks if a variable captured from a js prompt is less than 100. 
+// If it is, alert the user that their variable is less than 100. If it is not alert the user 
+// of what the value was and that it was greater than 100.
+// Three
+
+
+// Declare a function that takes a name as an argument and tells the user what name they've entered. 
+// Try running it after it has been declared.
+// Four
+
+
+
+// Declare a function that takes no arguments but prints "I am a great function" to the console. 
+// Try running it after it has been declared.
+// Five
+
+
+
+// Declare a function that lets you choose which door to open of 3 doors (like the TV game show). 
+// Behind one of the doors is a goat, behind another is a Jeep, and behind another is a can of worms. 
+// Alert the user which prize they got when a door number is passed in.
+// Six
